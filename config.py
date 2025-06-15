f55f1fe4cbd56641b5ba7fe2dd04261b130d058c
ducanhbui2004 <ducanhbui2004@gmail.com>
2025-06-15 21:15:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 FEATURES_PER_HAND = 21
 
 # NTên của mô hình đã được huấn luyện và lưu trữ
-MODEL_NAME = "vemohinh.pkl"
+MODEL_NAME = "final4.pkl"
 MODEL_CONFIDENCE = 0.5
 # Ngưỡng tự tin tối thiểu để phát hiện và theo dõi (confidence threshold)
 # Dùng cho MediaPipe để xác định có nhận diện được khuôn mặt, tay, v.v. hay không
